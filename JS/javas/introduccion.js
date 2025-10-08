06f9ae3948af9037681d50ff0431814e4e702269
jennysitap <jennycasti.mens07@gmail.com>
2025-10-08 13:38:52 -0700

    Animacion y diseño

--- JS/javas/introduccion.js
+++ JS/javas/introduccion.js
@@ -1,53 +1,32 @@
-let x=[1,2,3,4]
-const y = [20,50,...x]
-console.log(y)
-const edadLegalEEUU = 21
-const checkEstado = (edad, edadLegal) =>{
-    return edad >= edadLegal ? "Eres mayor" : "Eres menor"
-}
-const persona = checkEstado(18,edadLegalEEUU)
-console.log(persona)
-
-let arreglo=[
-    {id:1,name:"Jenny",age:20},
-    {id:2,name:"Magdiel",age:20},
-    {id:3,name:"Pamela",age:20},
-    {id:4,name:"Elisa",age:20},
-]
-let ul =document.getElementById("lista")
-var todo = ""
-arreglo.forEach((item)=>{
-    item.age = item.age * 10
-    todo += `
-    <li>${item.name} - Edad: ${item.age}</li>
-    `
-})
-console.log(todo)
-ul.innerHTML = todo
-
-
-console.log("Arreglo Original", arreglo);
-var copia = arreglo.map((item,index) =>{
-    item.age *=10
-    return item
-})
-console.log("Arreglo Modificado", arreglo);
-console.log("Arreglo Modificado", copia);
-let url ="https://thesimpsonsapi.com/api/"
-fetch(url+"characters").then(response=>{
-    if(!response.ok){
-        console.log("Doh, error en la red")
-    }
-    return response.json()
+var ul =document.getElementById("lista")
+let url= "https://thesimpsonsapi.com/api/"
+let arr_original =[]
+var lis=""
+var getlista= (page)=>{
+fetch(url+"characters?page="+page).then(response=>{
+      if(!response.ok){
+        console.log("Error en la red")
+      }
+      return response.json()
 }).then(data=>{
-    console.log("Datos del personaje: ", data)
-    var lis=""
+    console.log("Datos del personaje: ",data)
+    
     data.results.forEach(personaje=>{
-        lis+=`
-        <li>
-            <img src="https://cdn.thesimpsonsapi.com/500${personaje.portrait_path}" width="200px"/>
-            <h2>${personaje.name}</h2>
-        </li>`
+        arr_original.push({
+          personaje:personaje,
+          datos:null
+
+        })
+         lis+=`
+         <li>
+         <img src="https://cdn.thesimpsonsapi.com/500${personaje.portrait_path}" width="200px" />
+         <h2>${personaje.name}</h2>
+         </li>`
     })
     ul.innerHTML = lis
+    console.log("Arreglo Final",arr_original)
 })
+}
+for(let i=1; i<=5;i++){
+  getlista(i)
+}
